9dae12243522728e52ed35972db872f5dedd55de
hedon.ingadigital <hedon@ingadigital.com.br>
2022-08-07 12:40:48 -0300

    add estrutura da api

---
+++ Usuario/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Usuario.Model;
+
+namespace Usuario.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private static List<Usuarios> Usuarios()
+        {
+            return new List<Usuarios>
+            {
+                new Usuarios{Nome = "Hedon", Id = 1}
+            };
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(Usuarios());
+        }
+
+        [HttpPost]
+
+        public IActionResult Post(Usuarios usuarios)
+        {
+            var usuario = Usuarios();
+            usuario.Add(usuarios);
+            return Ok(usuario);
+        }
+    }
+}
---
+++ Usuario/Model/Usuarios.cs
@@ -0,0 +1,9 @@
+﻿namespace Usuario.Model
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento{ get; set; }
+    }
+}
